f3bb9d135b1ede8e3138afcbb3fad3ef50b538db
Jonathan Campo <joacampora@unal.edu.co>
2018-03-10 10:49:49 -0500

    Seeding the people

--- BackEnd/db/seeds.rb
+++ BackEnd/db/seeds.rb
@@ -5,3 +5,72 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Student.destroy_all
+100.times do |row|
+    Student.create(
+        cc: Faker::Number.unique.number(8),
+        first_name: Faker::Name.first_name,
+        last_name: Faker::Name.last_name,
+        e_mail: Faker::Internet.email, 
+        #created_at: Faker::Date.backward(370),
+        #updated_at: Faker::Date.between(1.year.ago, Date.today)
+      )
+    end
+
+puts 'finished loading student data'
+
+Teacher.destroy_all
+  100.times do |row|
+    Teacher.create(
+        cc: Faker::Number.unique.number(8),
+        first_name: Faker::Name.first_name,
+        last_name: Faker::Name.last_name,
+        e_mail: Faker::Internet.email, 
+        #created_at: Faker::Date.backward(370),
+        #updated_at: Faker::Date.between(1.year.ago, Date.today)
+      )
+  end
+
+puts 'finished loading teacher data'
+
+ExternalPerson.destroy_all
+50.times do |row|
+    ExternalPerson.create(
+        cc: Faker::Number.unique.number(8),
+        first_name: Faker::Name.first_name,
+        last_name: Faker::Name.last_name,
+        e_mail: Faker::Internet.email, 
+        #created_at: Faker::Date.backward(370),
+        #updated_at: Faker::Date.between(1.year.ago, Date.today)
+  )
+end
+
+puts 'finished loading external people data'
+
+HeadBuilding.destroy_all
+55.times do |row|
+    HeadBuilding.create(
+        cc: Faker::Number.unique.number(8),
+        first_name: Faker::Name.first_name,
+        last_name: Faker::Name.last_name,
+        e_mail: Faker::Internet.email, 
+        #created_at: Faker::Date.backward(370),
+        #updated_at: Faker::Date.between(1.year.ago, Date.today)
+      )
+end
+
+puts 'finished loading head building data'
+
+Manager.destroy_all
+55.times do |row|
+    Manager.create(
+        cc: Faker::Number.unique.number(8),
+        first_name: Faker::Name.first_name,
+        last_name: Faker::Name.last_name,
+        e_mail: Faker::Internet.email, 
+        #created_at: Faker::Date.backward(370),
+        #updated_at: Faker::Date.between(1.year.ago, Date.today)
+      )
+end
+
+puts 'finished loading manager data'
