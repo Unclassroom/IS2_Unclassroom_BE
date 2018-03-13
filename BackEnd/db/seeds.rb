1fdc11ccfda97962d4a46c84147d27a357434f6a
Jonathan Campo <joacampora@unal.edu.co>
2018-03-12 19:08:58 -0500

    gemfile

--- BackEnd/db/seeds.rb
+++ BackEnd/db/seeds.rb
@@ -5,9 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
 Student.destroy_all
 100.times do |row|
-    Student.create(
+    Student.create!(
         cc: Faker::Number.unique.number(8),
         first_name: Faker::Name.first_name,
         last_name: Faker::Name.last_name,
@@ -21,7 +23,7 @@ puts 'finished loading student data'
 
 Teacher.destroy_all
   100.times do |row|
-    Teacher.create(
+    Teacher.create!(
         cc: Faker::Number.unique.number(8),
         first_name: Faker::Name.first_name,
         last_name: Faker::Name.last_name,
@@ -35,7 +37,7 @@ puts 'finished loading teacher data'
 
 ExternalPerson.destroy_all
 50.times do |row|
-    ExternalPerson.create(
+    ExternalPerson.create!(
         cc: Faker::Number.unique.number(8),
         first_name: Faker::Name.first_name,
         last_name: Faker::Name.last_name,
@@ -49,7 +51,7 @@ puts 'finished loading external people data'
 
 HeadBuilding.destroy_all
 55.times do |row|
-    HeadBuilding.create(
+    HeadBuilding.create!(
         cc: Faker::Number.unique.number(8),
         first_name: Faker::Name.first_name,
         last_name: Faker::Name.last_name,
@@ -63,7 +65,7 @@ puts 'finished loading head building data'
 
 Manager.destroy_all
 55.times do |row|
-    Manager.create(
+    Manager.create!(
         cc: Faker::Number.unique.number(8),
         first_name: Faker::Name.first_name,
         last_name: Faker::Name.last_name,
@@ -77,7 +79,7 @@ puts 'finished loading manager data'
 
 Subject.destroy_all
 100.times do |row|
-        Subject.create(
+        Subject.create!(
         name: Faker::Educator.course, 
        # created_at: Faker::Date.backward(370),
        # updated_at: Faker::Date.between(1.year.ago, Date.today)
@@ -87,24 +89,23 @@ end
 puts 'finished loading subject data'
 
 Faculty.destroy_all
-Faculty.create(name: "Agronomia")
-Faculty.create(name: "Ciencias")
-Faculty.create(name: "Ciencias Económicas")
-Faculty.create(name: "Ciencias Humanas")
-Faculty.create(name: "Derecho, Ciencias Políticas y Sociales")
-Faculty.create(name: "Enfermería")
-Faculty.create(name: "Ingenieria")
-Faculty.create(name: "Medicina")
-Faculty.create(name: "Medicina Veterinaria y Zootecnia")
-Faculty.create(name: "Odontologia")
+Faculty.create!(name: "Agronomia")
+Faculty.create!(name: "Ciencias")
+Faculty.create!(name: "Ciencias Económicas")
+Faculty.create!(name: "Ciencias Humanas")
+Faculty.create!(name: "Derecho, Ciencias Políticas y Sociales")
+Faculty.create!(name: "Enfermería")
+Faculty.create!(name: "Ingenieria")
+Faculty.create!(name: "Medicina")
+Faculty.create!(name: "Medicina Veterinaria y Zootecnia")
+Faculty.create!(name: "Odontologia")
 
 puts 'finished loading faculty data'
 
 PurposeClassroom.destroy_all 
 100.times do |row|
-    PurposeClassroom.create(
+    PurposeClassroom.create!(
     name: Faker::Types.string,)
   end
 
-
-puts 'finished loading purpose classroom data'
+  puts 'finished loading purpose classroom data'
