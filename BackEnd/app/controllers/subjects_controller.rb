0600a71a8f5c3e219df232b1cda424b96817968d
gadrielag <adorados@unal.edu.co>
2018-03-15 09:08:29 -0500

    add models and controllers

--- BackEnd/app/controllers/subjects_controller.rb
+++ BackEnd/app/controllers/subjects_controller.rb
@@ -1,2 +1,22 @@
 class SubjectsController < ApplicationController
+    before_action :set_subjects, only: [:show, :update, :destroy]
+    def index
+        subjects = Subject.all
+        render json: subjects
+    end
+    def show
+       subjects = Subject.find(params[:id])
+       render json: subjects
+    end
+    def create
+        subject = Subject.new(subject_params)
+        if subject.save
+            render json: subject
+        else
+            render json: subject.errors
+        end
+    end
+    def subject_params
+        params.require(:subject).permit(:name)
+    end
 end
