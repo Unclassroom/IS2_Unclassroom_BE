7e0177ef126dd55b4e97ea171d780833a171ef94
gadrielag <adorados@unal.edu.co>
2018-03-19 10:11:56 -0500

    changed model

--- BackEnd/app/models/classroom.rb
+++ BackEnd/app/models/classroom.rb
@@ -6,7 +6,7 @@ class Classroom < ApplicationRecord
   belongs_to :type_classroom
   belongs_to :building
   belongs_to :deparment
-  has_many :classroom_events
-  has_many :classroom_schedules
+  has_many :classroom_events, through => :events
+  has_many :classroom_schedules, through => :schedules
   has_many :reports
 end
