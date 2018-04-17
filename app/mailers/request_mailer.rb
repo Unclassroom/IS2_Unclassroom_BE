c35f604ae17dc46de59cd9ba28cfa4e0ef749aa6
Luis Felipe Epia Realpe <lfepiar@unal.edu.co>
2018-04-16 19:33:35 -0500

    added mailer to request add

--- app/mailers/request_mailer.rb
+++ app/mailers/request_mailer.rb
@@ -1,2 +1,13 @@
 class RequestMailer < ApplicationMailer
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.request_mailer.new_request.subject
+  #
+  def new_request(request)
+    @request = request
+    @teacher = @request.teacher
+    mail to: @teacher.email, subject: "You just made request, great! "
+  end
 end
--- test/mailers/previews/request_mailer_preview.rb
+++ test/mailers/previews/request_mailer_preview.rb
@@ -1,4 +1,10 @@
 # Preview all emails at http://localhost:3000/rails/mailers/request_mailer
 class RequestMailerPreview < ActionMailer::Preview
 
+  # Preview this email at http://localhost:3000/rails/mailers/request_mailer/new_request
+  def new_request
+    request = Request.first
+    RequestMailer.new_request(request)
+  end
+
 end
