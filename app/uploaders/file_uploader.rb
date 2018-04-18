1356dea93ae3140a869dc025afe74133ec7b9a2e
Luis Felipe Epia Realpe <lfepiar@unal.edu.co>
2018-04-18 11:34:34 -0500

    mispeled pdf

--- app/uploaders/file_uploader.rb
+++ app/uploaders/file_uploader.rb
@@ -36,7 +36,7 @@ class FileUploader < CarrierWave::Uploader::Base
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
   def extension_whitelist
-    %w(jpg jpeg gif png txt pfd)
+    %w(jpg jpeg gif png txt pdf)
   end
 
   # Override the filename of the uploaded files:
