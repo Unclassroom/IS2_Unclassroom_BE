e6a13ecdea8d1b951c8544c8e0ffaab74417b178
gadrielag <adorados@unal.edu.co>
2018-05-02 00:14:18 -0500

    fix database

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 20180501221642) do
-=======
-ActiveRecord::Schema.define(version: 20180430041942) do
->>>>>>> develop
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
